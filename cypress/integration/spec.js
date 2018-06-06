e78c2309a6cf05f029a71d6ccd617df3431ef46d
hon2a <jkonopasek@wiser.com>
2018-06-06 12:37:20 +0200

    Use local render.

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,8 @@
 describe('pointer-events bug', () => {
   it('disallows clicking on elements with `pointer-events: none` children', () => {
-    cy.visit('https://ant.design/components/button/')
-    cy.get('.main-container .ant-btn').eq(0).click()
+    cy.window().then(({ document }) => {
+      document.body.innerHTML = '<button><span style="pointer-events: none">Some Label</span></button>'
+    })
+    cy.get('button').contains('Some Label').click()
   })
 })
