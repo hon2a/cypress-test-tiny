32e5016d4da6482807d83dbfba396c15d191e5ab
hon2a <hon2a@users.noreply.github.com>
2018-06-06 11:35:54 +0200

    Update spec.js

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,6 @@
-describe('page', () => {
-  it('works', () => {})
+describe('pointer-events bug', () => {
+  it('disallows clicking on elements with `pointer-events: none` children', () => {
+    cy.visit('https://ant.design/components/button/')
+    cy.get('.main-container .ant-btn').eq(0).click()
+  })
 })
